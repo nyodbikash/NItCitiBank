53be43cd40d847817f2dc4f2d3e306a5ad9e0279
Bikash Nayak <nbikash199@gmail.com>
2021-07-07 15:27:08 +0530

    added test.java

---
+++ DemoProj1/src/com/nt/test/Test.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+import com.nt.beans.Utillity;
+
+public class Test {
+	public static void main(String[] args) {
+		Utillity u=new Utillity();
+		System.out.println(u.getDate());
+	}
+
+}
