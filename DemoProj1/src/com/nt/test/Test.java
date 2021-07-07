161fc24282afcc5f5c48468644c229e5a673a8e1
Bikash Nayak <nbikash199@gmail.com>
2021-07-07 15:24:08 +0530

    commit

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -5,8 +5,8 @@ import com.nt.beans.Utillity;
 public class Test {
 	public static void main(String[] args) {
 		Utillity u=new Utillity();
-		
 		System.out.println("date is:"+u.getDate());
+		System.out.println(u.getDate());
 	}
 
 }
