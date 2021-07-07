1cbb677d0861295a984cc549be6ec55bb339c957
Bikash Nayak <nbikash199@gmail.com>
2021-07-07 14:20:00 +0530

    modified test.java

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Utillity;
+
 public class Test {
+	public static void main(String[] args) {
+		Utillity u=new Utillity();
+		System.out.println("date is:"+u.getDate());
+	}
 
 }
